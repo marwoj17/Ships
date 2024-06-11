ddd18ccaf639ddc8e017a9885da7361ba0320b19
marwoj17 <max.wojcikiewicz666@gmail.com>
2024-06-11 09:24:26 +0200

    Add files via upload

--- Ships/Ships/Program.cs
+++ Ships/Ships/Program.cs
@@ -30,6 +30,27 @@ class Program
         }
     }
 
+    static void PrintBoardsSideBySide(char[,] board1, char[,] board2)
+    {
+        Console.WriteLine("    Twoja plansza                    Plansza przeciwnika");
+        Console.WriteLine("  A B C D E F G H I J      A B C D E F G H I J");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write((i + 1).ToString().PadLeft(2) + " ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(board1[i, j] + " ");
+            }
+            Console.Write("   ");
+            Console.Write((i + 1).ToString().PadLeft(2) + " ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(board2[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
     static bool PlaceShip(char[,] board, int x, int y, int length, bool horizontal)
     {
         if (horizontal)
@@ -61,7 +82,7 @@ class Program
 
     static void PlaceAllShips(char[,] board)
     {
-        var shipSizes = new List<int> { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+        var shipSizes = new List<int> { 1 };
         foreach (var size in shipSizes)
         {
             bool placed = false;
@@ -73,8 +94,23 @@ class Program
                 string pos = Console.ReadLine();
                 int x = int.Parse(pos.Substring(1)) - 1;
                 int y = pos[0] - 'A';
-                Console.Write("Podaj orientację (h dla poziomej, v dla pionowej): ");
-                string orientation = Console.ReadLine();
+
+                string orientation = "";
+                bool validOrientation = false;
+                while (!validOrientation)
+                {
+                    Console.Write("Podaj orientację (h dla poziomej, v dla pionowej): ");
+                    orientation = Console.ReadLine();
+                    if (orientation == "h" || orientation == "v")
+                    {
+                        validOrientation = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidłowa orientacja, spróbuj ponownie.");
+                    }
+                }
+
                 placed = PlaceShip(board, x, y, size, orientation == "h");
                 if (!placed) Console.WriteLine("Błąd ustawienia statku, spróbuj ponownie.");
             }
@@ -118,28 +154,59 @@ class Program
         Console.Clear();
     }
 
+    class PlayerManager
+    {
+        private int currentPlayer;
+
+        public PlayerManager()
+        {
+            currentPlayer = 1;
+        }
+
+        public void SwitchPlayer()
+        {
+            Console.WriteLine($"Gracz {currentPlayer} zakończył swoją turę.");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować.");
+            Console.ReadKey();
+            Console.Clear();
+            currentPlayer = currentPlayer == 1 ? 2 : 1;
+            Console.WriteLine($"Gracz {currentPlayer} ustaw swoje statki.");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby rozpocząć ustawianie.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public int GetCurrentPlayer()
+        {
+            return currentPlayer;
+        }
+    }
+
     static void Main(string[] args)
     {
         char[,] board1 = CreateEmptyBoard();
         char[,] board2 = CreateEmptyBoard();
         char[,] targetBoard1 = CreateEmptyBoard();
         char[,] targetBoard2 = CreateEmptyBoard();
+        PlayerManager playerManager = new PlayerManager();
 
         Console.WriteLine("Gracz 1 ustaw swoje statki.");
         PlaceAllShips(board1);
-        Console.Clear();
+        playerManager.SwitchPlayer();
+
         Console.WriteLine("Gracz 2 ustaw swoje statki.");
         PlaceAllShips(board2);
-        Console.Clear();
+        playerManager.SwitchPlayer();
 
         bool player1Turn = true;
         while (true)
         {
+            char[,] playerBoard = player1Turn ? board1 : board2;
             char[,] targetBoard = player1Turn ? targetBoard1 : targetBoard2;
             char[,] enemyBoard = player1Turn ? board2 : board1;
 
-            Console.WriteLine($"Tura gracza {(player1Turn ? 1 : 2)}");
-            PrintBoard(targetBoard);
+            Console.WriteLine($"Tura gracza {playerManager.GetCurrentPlayer()}");
+            PrintBoardsSideBySide(playerBoard, targetBoard);
             Console.Write("Podaj pozycję do ataku (np. A5): ");
             string pos = Console.ReadLine();
             int x = int.Parse(pos.Substring(1)) - 1;
@@ -151,13 +218,14 @@ class Program
 
             if (CheckWin(enemyBoard))
             {
-                Console.WriteLine($"Gracz {(player1Turn ? 1 : 2)} wygrywa!");
+                Console.WriteLine($"Gracz {playerManager.GetCurrentPlayer()} wygrywa!");
                 Console.WriteLine("Gratulacje!");
                 break;
             }
 
             NextPlayer();
             player1Turn = !player1Turn;
+            playerManager.SwitchPlayer();
         }
     }
 }
